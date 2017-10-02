6d7059bfd307e76791945c9da648c24e5da0bf51
troper <troper107@gmail.com>
2017-10-02 19:42:57 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+                Scanner keyboard = new Scanner(System.in);
+                String myName;
+
+                System.out.println("What's your name?");
+
+                myName = keyboard.nextLine();
+
+                System.out.println("Nice to meet you, " + myName);
+            }
+/*
+* Taylor Roper
+* Age: 19
+ */
+
+    }
+
